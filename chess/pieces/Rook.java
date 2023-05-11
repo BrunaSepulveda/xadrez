de8d7719fafd091f5d6e416778b9ca4c9dfc5697
BrunaSepulveda <sepulvedabruna@gmail.com>
2023-05-11 16:43:20 -0300

    ajuste para exibir movimentos possíveis para torre

--- chess/pieces/Rook.java
+++ chess/pieces/Rook.java
@@ -1,5 +1,7 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+
 import boardGame.Board;
 import boardGame.Position;
 import chess.ChessPiece;
@@ -15,53 +17,51 @@ public class Rook extends ChessPiece {
 	public String toString() {
 		return "R";
 	}
-	
+
+	private ArrayList<int[]> straightMotion(boolean isItInTheColumn, boolean isItSum, Position currentPosition){
+		ArrayList<int[]> positionsBoo = new ArrayList<int[]>();
+		if (isItInTheColumn) {
+			currentPosition.setValues(position.getRow(), position.getColumn() + (isItSum ? 1 : -1));
+		} else {
+			currentPosition.setValues(position.getRow() + (isItSum ? 1 : -1), position.getColumn());
+		}
+			while (getBoard().positionExists(currentPosition) && !getBoard().thereIsAPiece(currentPosition)) {
+				int [] truePositon = {currentPosition.getRow(), currentPosition.getColumn()};
+				positionsBoo.add(truePositon);
+				if (isItInTheColumn) {
+					currentPosition.setColumn(currentPosition.getColumn() + (isItSum ? 1 : -1));
+				} else {
+					currentPosition.setRow(currentPosition.getRow()  + (isItSum ? 1 : -1));
+				}
+			}
+			if (getBoard().positionExists(currentPosition) && isThereOpponentPiece(currentPosition)) {
+				int [] truePositon = {currentPosition.getRow(), currentPosition.getColumn()};
+				positionsBoo.add(truePositon);
+			}
+			return positionsBoo;
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
 		Position p = new Position(0, 0);
-		
-		// above
-		p.setValues(position.getRow() - 1, position.getColumn());
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() - 1);
+		ArrayList<int[]> allPossibleMoves = new ArrayList<>();
+		//above
+		ArrayList<int[]> abovePositions =  straightMotion(false,false,p);
+		allPossibleMoves.addAll(abovePositions);
+		//left
+		ArrayList<int[]> leftPositions = straightMotion(true,false,p);
+		allPossibleMoves.addAll(leftPositions);
+		//right
+		ArrayList<int[]> rightPositions = straightMotion(true,true,p);
+		allPossibleMoves.addAll(rightPositions);
+		//below
+		ArrayList<int[]> belowPositions = straightMotion(false,true,p);
+		allPossibleMoves.addAll(belowPositions);
+
+		for (int[] positionTrue : allPossibleMoves) {
+			mat[positionTrue[0]][positionTrue[1]] = true;
 		}
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		// left
-		p.setValues(position.getRow(), position.getColumn() - 1);
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);
-		}
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		// right
-		p.setValues(position.getRow(), position.getColumn() + 1);
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() + 1);
-		}
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
-		// below
-		p.setValues(position.getRow() + 1, position.getColumn());
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() + 1);
-		}
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
 		return mat;
 	}
 }
